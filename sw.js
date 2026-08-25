26570c8601924c9d954f3fca16608522454def3c
Biggpaul85 <paulovitor19855@gmail.com>
2026-08-25 10:39:41 -0300

    Primeiro deploy

---
+++ sw.js
@@ -0,0 +1,60 @@
+/* VITTO GD - Service Worker */
+const CACHE = 'vitto-gd-v2';
+const CORE = [
+  './',
+  './index.html',
+  './produtos.js',
+  './manifest.json',
+  './img/icon-192.png',
+  './img/icon-512.png',
+  './img/icon-maskable-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(CORE)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  const url = new URL(req.url);
+  if (url.origin !== location.origin) return;
+
+  // Navegação: rede primeiro, cai pro cache offline
+  if (req.mode === 'navigate') {
+    e.respondWith(
+      fetch(req)
+        .then((res) => {
+          const copy = res.clone();
+          caches.open(CACHE).then((c) => c.put('./index.html', copy));
+          return res;
+        })
+        .catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+
+  // Demais recursos: cache primeiro, atualiza em background
+  e.respondWith(
+    caches.match(req).then((hit) => {
+      const fetchPromise = fetch(req).then((res) => {
+        if (res && res.status === 200) {
+          const copy = res.clone();
+          caches.open(CACHE).then((c) => c.put(req, copy));
+        }
+        return res;
+      }).catch(() => hit);
+      return hit || fetchPromise;
+    })
+  );
+});
